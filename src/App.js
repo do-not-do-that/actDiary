08840194c3ac3a6601be9a3c84674ebb684015fe
do-not-do-that <rl7968@naver.com>
2023-01-10 23:35:51 +0900

    feat : 리스트 배열 다루기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: '테스터',
+    content: 'hi! 테스트입니다',
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: '테스터훈',
+    content: 'hi! 오늘은 오징어 튀김',
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: '오킹',
+    content: '굿바이 짜장면',
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
