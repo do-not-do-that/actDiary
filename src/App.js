5c28518cdd0a4436505dc5671f764eae3ee5f937
do-not-do-that <rl7968@naver.com>
2023-01-17 12:18:20 +0900

    feat : fetch 를 이용한 fake data 가져와 일기 기본 세팅하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import { useState, useRef } from 'react';
-import Lifecycle from './Lifecycle';
+import { useState, useRef, useEffect } from 'react';
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
-  const dateId = useRef(0);
+  const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      'https://jsonplaceholder.typicode.com/comments'
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
@@ -14,9 +38,9 @@ function App() {
       content,
       emotion,
       created_date,
-      id: dateId.current,
+      id: dataId.current,
     };
-    dateId.current += 1;
+    dataId.current += 1;
     setData([newItem, ...data]);
   };
 
@@ -34,7 +58,6 @@ function App() {
   };
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
